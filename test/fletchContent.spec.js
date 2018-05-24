97c9232ff3f2e3e374bb52ef0cff7963d915c62b
Jamie.Brown <jamiebrown201@hotmail.com>
2018-05-24 16:22:38 +0100

    blank test

--- test/fletchContent.spec.js
+++ test/fletchContent.spec.js
@@ -1,6 +1,15 @@
-const sinon = require('sinon');
-const chai = require('chai');
-const proxyquire = require('proxyquire');
-const expect = chai.expect;
+const chai = require("chai");
+const expect = require("chai").expect;
+const nock = require("nock");
+const subject = require("../lib/fetchContent.js");
 
-const sandbox = sinon.sandbox.create();
+const SAPI_PATH = "http://api.ft.com/content/search/v1";
+const CAPI_KEY = process.env.CAPI_KEY;
+
+describe("lib/fetchContent", () => {
+  beforeEach(() => {
+    nock("http://api.ft.com/")
+      .get(`${SAPI_PATH}?apiKey=${CAPI_KEY}`)
+      .reply(200, {});
+  });
+});

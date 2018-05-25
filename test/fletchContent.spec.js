1270a29156205dc61a10ec09c5425ce28e4169a9
Jamie.Brown <jamiebrown201@hotmail.com>
2018-05-25 15:17:52 +0100

    200 tests search

--- test/fletchContent.spec.js
+++ test/fletchContent.spec.js
@@ -1,15 +1,28 @@
 const chai = require("chai");
-const expect = require("chai").expect;
+const expect = chai.expect;
 const nock = require("nock");
 const subject = require("../lib/fetchContent.js");
+const searchFixture = require("./fixtures/searchResponse.json");
 
-const SAPI_PATH = "http://api.ft.com/content/search/v1";
 const CAPI_KEY = process.env.CAPI_KEY;
+const searchTerm = { queryString: "John Dalli", testParam: "test" };
+
+const requestBody =
+  '{"queryString":"John Dalli","queryContext":{"curations":["ARTICLES","BLOGS"]},"resultContext":{"maxResults":"10","offset":"0","aspects":["title","lifecycle","location"],"sortOrder":"DESC","sortField":"lastPublishDateTime","facets":{"names":["people","organisations","topics"],"maxElements":-1}}}';
 
 describe("lib/fetchContent", () => {
-  beforeEach(() => {
-    nock("http://api.ft.com/")
-      .get(`${SAPI_PATH}?apiKey=${CAPI_KEY}`)
-      .reply(200, {});
+  context("for a 200 response", () => {
+    beforeEach(() => {
+      nock("http://api.ft.com")
+        .post(`/content/search/v1?apiKey=${CAPI_KEY}`, requestBody)
+        .reply(200, searchFixture);
+    });
+
+    it("makes a request to the search api", () => {
+      subject.search(searchTerm).then(data => {
+        expect(nock.isDone()).to.be.true;
+        nock.cleanAll();
+      });
+    });
   });
 });

cbca748d8ee0358ba5a9a226fc78b3f1ce7f1ad9
Jamie.Brown <jamiebrown201@hotmail.com>
2018-05-29 11:52:44 +0100

    resultsContext params

--- test/fixtures/responseBody.js
+++ test/fixtures/responseBody.js
@@ -1,10 +1,32 @@
-const constraints =
-  '{"queryString":"\\"John Dalli\\" and TEST and TEST2","queryContext":{"curations":["ARTICLES","BLOGS"]},"resultContext":{"maxResults":"10","offset":"0","aspects":["title","lifecycle","location"],"sortOrder":"DESC","sortField":"lastPublishDateTime","facets":{"names":["people","organisations","topics"],"maxElements":-1}}}';
+const defualt = {
+  queryString: "John Dalli",
+  queryContext: { curations: ["ARTICLES", "BLOGS"] },
+  resultContext: {
+    maxResults: "10",
+    offset: "0",
+    aspects: ["title", "lifecycle", "location"],
+    sortOrder: "DESC",
+    sortField: "lastPublishDateTime",
+    facets: { names: ["people", "organisations", "topics"], maxElements: -1 }
+  }
+};
 
-const defualt =
-  '{"queryString":"John Dalli","queryContext":{"curations":["ARTICLES","BLOGS"]},"resultContext":{"maxResults":"10","offset":"0","aspects":["title","lifecycle","location"],"sortOrder":"DESC","sortField":"lastPublishDateTime","facets":{"names":["people","organisations","topics"],"maxElements":-1}}}';
+const constraints = JSON.stringify({
+  ...defualt,
+  queryString: '"John Dalli" and TEST and TEST2'
+});
+
+const resultContext = JSON.stringify({
+  ...defualt,
+  resultContext: {
+    ...defualt.resultContext,
+    maxResults: "11",
+    offset: "1"
+  }
+});
 
 module.exports = {
   constraints,
+  resultContext,
   defualt
 };
--- test/fletchContent.spec.js
+++ test/fletchContent.spec.js
@@ -9,32 +9,49 @@ const CAPI_KEY = process.env.CAPI_KEY;
 const defaultSearchTerm = { queryString: "John Dalli" };
 
 describe("lib/fetchContent", () => {
-  context("constructing the request", () => {
-    afterEach(() => {
-      nock.cleanAll();
-    });
-
-    it("requests the search api with a queryString", done => {
-      nock("http://api.ft.com")
-        .post(`/content/search/v1?apiKey=${CAPI_KEY}`, responseBody.defualt)
-        .reply(200, searchFixture);
-      subject.search(defaultSearchTerm).then(data => {
-        expect(nock.isDone()).to.be.true;
-        done();
+  describe("search", () => {
+    context("constructing the request", () => {
+      afterEach(() => {
+        nock.cleanAll();
       });
-    });
 
-    it("adds constraints to queryString", done => {
-      const constraintsSearchTerm = {
-        ...defaultSearchTerm,
-        constraints: ["TEST", "TEST2"]
-      };
-      nock("http://api.ft.com")
-        .post(`/content/search/v1?apiKey=${CAPI_KEY}`, responseBody.constraints)
-        .reply(200, searchFixture);
-      subject.search(constraintsSearchTerm).then(data => {
+      it("requests the search api with a queryString", async () => {
+        nock("http://api.ft.com")
+          .post(`/content/search/v1?apiKey=${CAPI_KEY}`, responseBody.defualt)
+          .reply(200, searchFixture);
+        await subject.search(defaultSearchTerm);
         expect(nock.isDone()).to.be.true;
-        done();
+      });
+
+      it("adds constraints to queryString", async () => {
+        const constraintsSearchTerm = {
+          ...defaultSearchTerm,
+          constraints: ["TEST", "TEST2"]
+        };
+        nock("http://api.ft.com")
+          .post(
+            `/content/search/v1?apiKey=${CAPI_KEY}`,
+            responseBody.constraints
+          )
+          .reply(200, searchFixture);
+        await subject.search(constraintsSearchTerm);
+        expect(nock.isDone()).to.be.true;
+      });
+
+      it("overrides default resultContext when supplied", async () => {
+        const resultContext = {
+          ...defaultSearchTerm,
+          maxResults: 11,
+          offset: 1
+        };
+        nock("http://api.ft.com")
+          .post(
+            `/content/search/v1?apiKey=${CAPI_KEY}`,
+            responseBody.resultContext
+          )
+          .reply(200, searchFixture);
+        await subject.search(resultContext);
+        expect(nock.isDone()).to.be.true;
       });
     });
   });

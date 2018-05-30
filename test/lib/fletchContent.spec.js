a06caa0fb69062672ad009ba8c6442bcb75c7e99
Jamie.Brown <jamiebrown201@hotmail.com>
2018-05-30 14:17:04 +0100

    add content check

--- test/lib/fletchContent.spec.js
+++ test/lib/fletchContent.spec.js
@@ -4,11 +4,14 @@ const nock = require("nock");
 const subject = require("../../lib/fetchContent.js");
 const responseBody = require("../fixtures/search/responseBody");
 const searchFixture = require("../fixtures/search/searchResponse.json");
+const newsFixture = require("../fixtures/getArticle/newsResponse.json");
 
 const CAPI_KEY = process.env.CAPI_KEY;
 
 const defaultSearchTerm = { queryString: "John Dalli" };
 const newsId = "70fc3c0e-1cb5-11e8-956a-43db76e69936";
+
+// need to add response checking to search
 
 describe("lib/fetchContent", () => {
   afterEach(() => {
@@ -16,13 +19,14 @@ describe("lib/fetchContent", () => {
   });
 
   describe("search", () => {
-    context("constructing the request", () => {
+    context("for a 200 repsonse", () => {
       it("requests the search api with a queryString", async () => {
         nock("http://api.ft.com")
           .post(`/content/search/v1?apiKey=${CAPI_KEY}`, responseBody.defualt)
           .reply(200, searchFixture);
-        await subject.search(defaultSearchTerm);
+        const search = await subject.search(defaultSearchTerm);
         expect(nock.isDone()).to.be.true;
+        expect(search.sapiObj).to.not.be.undefined;
       });
 
       it("adds constraints to queryString", async () => {
@@ -36,8 +40,9 @@ describe("lib/fetchContent", () => {
             responseBody.constraints
           )
           .reply(200, searchFixture);
-        await subject.search(constraintsSearchTerm);
+        const search = await await subject.search(constraintsSearchTerm);
         expect(nock.isDone()).to.be.true;
+        expect(search.sapiObj).to.not.be.undefined;
       });
 
       it("overrides default resultContext when supplied", async () => {
@@ -52,8 +57,9 @@ describe("lib/fetchContent", () => {
             responseBody.resultContext
           )
           .reply(200, searchFixture);
-        await subject.search(resultContext);
+        const search = await await subject.search(resultContext);
         expect(nock.isDone()).to.be.true;
+        expect(search.sapiObj).to.not.be.undefined;
       });
 
       context("with errors", () => {
@@ -79,18 +85,39 @@ describe("lib/fetchContent", () => {
   describe("getArticle", () => {
     context("for a 200 repsonse", () => {
       beforeEach(() => {
-        nock("https://api.ft.com")
-          .get(`/enrichedcontent/${newsId}?apiKey=${process.env.CAPI_KEY}`)
+        nock("http://api.ft.com")
+          .get(`/enrichedcontent/${newsId}?apiKey=${CAPI_KEY}`)
           .reply(200, newsFixture);
       });
 
-      it("returns an object", () => {
-        return subject.getArticle(newsId).then(res => {
-          expect(nock.isDone()).to.be.true;
-          expect(res.title).to.equal(
-            "PSA chief demands fairness on emissions penalties"
+      it("returns an object", async () => {
+        const article = await subject.getArticle(newsId);
+        expect(nock.isDone()).to.be.true;
+        expect(article.title).to.equal(
+          "PSA chief demands fairness on emissions penalties"
+        );
+      });
+    });
+    context("error response", () => {
+      beforeEach(() => {
+        nock("http://api.ft.com")
+          .get(`/enrichedcontent/${newsId}?apiKey=${process.env.CAPI_KEY}`)
+          .reply(400, "Forbidden");
+      });
+
+      it("throws an error", async () => {
+        try {
+          const article = await subject.getArticle(newsId);
+          expect.fail(
+            null,
+            `ERROR: fetch article for uuid=${newsId} status code=400`,
+            "This should not have resolved"
           );
-        });
+        } catch (err) {
+          expect(err).to.equal(
+            `ERROR: fetch article for uuid=${newsId} status code=400`
+          );
+        }
       });
     });
   });
